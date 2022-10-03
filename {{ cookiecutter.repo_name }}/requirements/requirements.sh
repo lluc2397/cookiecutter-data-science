34bd2237c9ecb2a8a3e708c3b489427625e50617
Lucas <lluc23@hotmail.com>
2022-10-03 12:51:26 +0200

    improve config

---
+++ {{ cookiecutter.repo_name }}/requirements/requirements.sh
@@ -0,0 +1,5 @@
+virtualenv temp_venv
+$1 install -U pip
+$1 install -r requirements/base.txt
+$1 freeze > requirements/requirements.txt
+$1 list --outdated --format=columns > requirements/outdated.txt
